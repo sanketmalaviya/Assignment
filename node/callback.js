732663da66ef59bda36bbffbe5d2059e273f50b9
sanketmalaviya <sanketmalaviya22@gmail.com>
2023-01-31 10:16:56 +0530

    moudle 2

---
+++ node/callback.js
@@ -0,0 +1,21 @@
+ 
+ const square = (a,callback) => {
+    callback(a*a)
+ }
+
+ const cube = (b,callback) => {
+    callback(b*b*b)
+ }
+ const mul = (d,callback)=>{
+    callback(d * 10)
+ }
+
+ square(10,(square)=>{
+    console.log(square);
+        cube(square,(c)=>{
+            console.log(c);
+                mul(c,(m)=>{
+                    console.log(m);
+                })
+        })              
+ })
