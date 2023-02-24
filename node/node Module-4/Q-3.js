ca76c7f1a26ec1f0c3d9e4fb274f621a745391d6
sanketmalaviya <sanketmalaviya22@gmail.com>
2023-02-24 13:07:35 +0530

    node

--- node/node Module-4/Q-3.js
+++ node/node Module-4/Q-3.js
@@ -1,11 +1,12 @@
 const mongodb = require("mongodb")
 const MongoClient = mongodb.MongoClient
 
-const url_DB = " mongodb://127.0.0.1:27017"
+const Db_Url = "mongodb://127.0.0.1:27017"
 const database = "online_shopping_App"
 
-MongoClient.connect(url_DB).then(result =>{
-    console.log("databse connected");
+MongoClient.connect(Db_Url).then(result =>{
+    console.log("database connected");
 }).catch(err =>{
-    console.log(err);
-})
+        console.log(err);
+    })
+
