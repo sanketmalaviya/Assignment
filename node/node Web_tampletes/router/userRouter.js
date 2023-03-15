d18772b99301a973d25dac724d96d845b1f914d4
sanketmalaviya <sanketmalaviya22@gmail.com>
2023-03-15 16:36:04 +0530

    node

--- node/node Web_tampletes/router/userRouter.js
+++ node/node Web_tampletes/router/userRouter.js
@@ -1,44 +1,48 @@
-const express = require("express")
-const router = express.Router()
-const User = require("../model/user")
-const bcrypt = require("bcryptjs")
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+
 router.get("/", (req, resp) => {
-    resp.render("registration")
-})
-router.get("/login",(req,resp)=>{
-    resp.render("login")
-})
-router.post("/adduser", async (req,resp)=> {
-    try {
-        const user = new User(req.body)
-        const data = await user.save();
-        resp.render("registration", { msg: "Registration success !!!!" })
-    } catch (error) {
-        
+  resp.render("registration");
+});
+
+router.get("/login", (req, resp) => {
+  resp.render("login");
+});
+
+router.post("/adduser", async (req, resp) => {
+  try {
+    const user = new User(req.body);
+    const data = await user.save();
+    resp.render("registration", { msg: "Registration success !!!!" });
+  } catch (error) {}
+});
+
+router.post("/userlogin", async (req, resp) => {
+  try {
+    const useremail = req.body.email;
+    const userpass = req.body.pass;
+
+    const userdata = await User.findOne({ email: useremail });
+    const valid = await bcrypt.compare(userpass, userdata.pass);
+    if (valid) {
+      const token = await jwt.sign({ _id: userdata.id }, process.env.Skey);
+      resp.cookie("jwt", token);
+      resp.render("home");
+    } else {
+      resp.render("404 not found");
     }
-})
+  } catch (error) {
+    resp.render("error");
+  }
+});
 
-router.post("/userlogin", async (req,resp)=>{
-    try {
-        const useremail = req.body.email
-        const userpass = req.body.pass
-
-        const userdata = await User.findOne({email: useremail})
-        const valid = await bcrypt.compare(userpass,userdata.pass)
-        if (valid) {
-            resp.render("home")
-        }
-        else{
-            resp.render("404 not found")
-        }
-    } catch (error) {
-        resp.render("error")
-    }
-})
+router.get("/home", auth, (req, resp) => {
+  resp.render("home");
+});
 
 
-
-router.get("/home", (req, resp) => {
-    resp.render("home")
-})
-module.exports = router
+module.exports = router;
